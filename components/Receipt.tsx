4426374cb9229c0fc9a7713beeccdcca02c97904
okinboxme <Okinboxme@gmail.com>
2024-06-18 10:13:39 +0300

    Update Receipt.tsx

--- components/Receipt.tsx
+++ components/Receipt.tsx
@@ -14,6 +14,62 @@ interface ReceiptProps {
 const Receipt: React.FC<ReceiptProps> = ({ order, total }) => {
   return (
     <div>
+      <table class="receipt-table">
+  <thead>
+    <tr>
+      <th>Date & Time</th>
+      <th>[PIN]</th> <th>Invoice No.</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>[Date & Time]</td> <td>[PIN]</td> <td>[Invoice Serial Number]</td> </tr>
+  </tbody>
+</table>
+
+<table class="receipt-table">
+  <thead>
+    <tr>
+      <th>Description</th>
+      <th>HS Code</th>
+      <th>Qty</th>
+      <th>Unit</th>
+      <th>Rate</th>
+      <th>Amount (Excl. Tax)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Item 1</td>
+      <td>[HS Code]</td> <td>1</td>
+      <td>Unit</td>
+      <td>KSH 10.00</td>
+      <td>KSH 10.00</td>
+    </tr>
+    <tr>
+      <td>Item 2</td>
+      <td>[HS Code]</td> <td>2</td>
+      <td>Unit</td>
+      <td>KSH 20.00</td>
+      <td>KSH 40.00</td>
+    </tr>
+  </tbody>
+</table>
+
+<table class="receipt-table">
+  <thead>
+    <tr>
+      <th colspan="5"></th>
+      <th>Sub-Total</th>
+      <th>Tax</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td colspan="5"></td>
+      <td>KSH 50.00</td>  <td>KSH 8.00 (16%)</td>  </tr>
+  </tbody>
+</table>
       <ul>
         {order.map((item) => (
           <li key={item.id}>
